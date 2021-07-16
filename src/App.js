153fdb8f4e758a7ac195b1daa9f818950a846ff3
ggneiracc68 <ggneiracc68@gmail.com>
2021-07-16 10:24:18 -0500

    Versión 3. Se agrego componente menu de navegación y img tipo URL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+
+import MenuNav from "./sitioweb/MenuNav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Esta es una pagina de prueba... Que la misma estara en la nube...
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="container">
+	<header className="row bg-secondary">
+		<img src="https://i.ibb.co/gzdmNPk/header.jpg" alt="header" border="0" />
+	</header>
+
+	<nav className="row bg-warning">
+		<MenuNav />
+	</nav>
+
+	<section className="row bg-primary"> 
+		<article className="col-md-6 bg-warning"> 
+			Articulo 1
+		</article>
+		
+		<article className="col-md-4 bg-secondary">
+			Articulo 2
+		</article>
+
+		<aside className="col-md-2 bg-primary">
+			aside (Apartado)
+		</aside>
+	</section>
+
+	<footer className="row bg-dark text-light">
+		footer (Pie de página)
+	</footer>
+</div>
   );
 }
 
